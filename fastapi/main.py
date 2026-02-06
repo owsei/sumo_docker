f3c970cbc02abbd7ef367762394d635d5a4b0181
Pedro <owsei@yahoo.com>
2026-02-06 09:40:05 +0100

    changes to show entities

--- fastapi/main.py
+++ fastapi/main.py
@@ -232,4 +232,4 @@ async def start_simulation(bbox: BoundingBox):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
