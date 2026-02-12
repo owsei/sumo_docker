fecd4955d425a4f6186cf9a2bf5ed215e62df636
Pedro <owsei@yahoo.com>
2026-02-12 11:20:48 +0100

    close streets

--- fastapi/main.py
+++ fastapi/main.py
@@ -18,6 +18,7 @@ from sumolib import checkBinary
 import traci
 from urllib.parse import unquote
 import platform
+import asyncio
 
 app = FastAPI()
 
@@ -72,55 +73,6 @@ def download_osm_data(bbox: BoundingBox, output_path: str):
         print("Error al conectar con Overpass: ", response.status_code)
         raise Exception(f"Error al conectar con Overpass: {response.status_code}")
 
-def generate_net(sumo_home ,bbox):
-    
-    osm_file = os.path.join(tmpdir, "mapa.osm.xml")
-    net_file = os.path.join(tmpdir, "mapa.net.xml")
-    route_file = os.path.join(tmpdir, "mapa.rou.xml")
-    
-    # 1. Descarga (usando el método de requests que vimos antes)
-    download_osm_data(bbox, osm_file)
-    sumo_types = ",".join([f"highway.{t}" for t in bbox.road_types])
-    
-    # 2. Generar red de SUMO
-    subprocess.run([
-        os.path.join(sumo_home, "bin", "netconvert"),
-        "--osm-files", osm_file,
-        "--output-file", net_file,
-        "--geometry.remove", "true",
-        "--proj.utm", "true",
-        "--keep-edges.by-type", sumo_types, # <--- Mantiene solo estos tipos
-        "--remove-edges.isolated", "true"
-    ], check=True)
-
-def generate_traffic(sumo_home, net_file, route_file):
-    random_trips = os.path.join(sumo_home, "tools", "randomTrips.py")
-    subprocess.run([
-        "python", random_trips,
-        "-n", net_file,
-        "-r", route_file,
-        "-e", "3600",  # Simular 3600 segundos de tráfico
-        "--period", "10" # Aparece un coche cada 0.5 segundos
-    ], check=True)
-    
-def wait_for_file(filepath, timeout=10):
-    """
-    Bloquea la ejecución hasta que el archivo existe y es accesible.
-    """
-    start_time = time.time()
-    while time.time() - start_time < timeout:
-        if os.path.exists(filepath):
-            try:
-                # Intentamos abrirlo en modo lectura para verificar que no está bloqueado
-                with open(filepath, 'r') as f:
-                    if len(f.read(1)) > 0: # Verificamos que no esté vacío
-                        return True
-            except IOError:
-                # El archivo existe pero está siendo usado por otro proceso
-                pass
-        time.sleep(0.1) # Espera de 100ms antes de reintentar
-    return False
-
 def convert_net_to_geojson_net(net_file):
     """
     Usa sumolib para leer la red de SUMO y crear un GeoJSON 
@@ -135,12 +87,11 @@ def convert_net_to_geojson_net(net_file):
         # Convertimos las coordenadas internas de SUMO a Lon/Lat
         shape = edge.getShape()
         coords = [net.convertXY2LonLat(x, y) for x, y in shape]
-
         # Extraemos las propiedades que queremos
         # Nota: edge.getName() devuelve el nombre de la calle de OSM
         properties = {
             "id": edge.getID(),
-            "nombre": edge.getName() or "Calle sin nombre",
+            "nombre": edge.getStreetName() or "Calle sin nombre",
             "tipo": edge.getType(),
             "velocidad_max": edge.getSpeed() * 3.6, # Convertir m/s a km/h
             "carriles": edge.getLaneNumber()
@@ -161,53 +112,6 @@ def convert_net_to_geojson_net(net_file):
         "features": features
     }
 
-def convert_to_geojson_traci(simulation_data: List[Dict]) -> Dict:
-    """Convierte los datos de simulación a GeoJSON para Cesium"""
-    features = []
-    
-    for step_data in simulation_data:
-        for vehicle in step_data["vehicles"]:
-            feature = {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [vehicle["longitude"], vehicle["latitude"]]
-                },
-                "properties": {
-                    "id": vehicle["id"],
-                    "speed": vehicle["speed"],
-                    "angle": vehicle["angle"],
-                    "time": vehicle["time"]
-                }
-            }
-            features.append(feature)
-    
-    return {
-        "type": "FeatureCollection",
-        "features": features
-    }
-
-async def convert_to_websocket(simulation_data: List[Dict]) -> Dict:
-    """Convierte los datos de simulación a GeoJSON para Cesium"""
-    features = []
-    
-    for step_data in simulation_data:
-        for vehicle in step_data["vehicles"]:
-            feature = {
-                "type": "Feature",
-                "geometry": {
-                    "type": "Point",
-                    "coordinates": [vehicle["longitude"], vehicle["latitude"]]
-                },
-                "properties": {
-                    "id": vehicle["id"],
-                    "speed": vehicle["speed"],
-                    "angle": vehicle["angle"],
-                    "time": vehicle["time"]
-                }
-            }
-            await websocket.send_json({feature})  
-    
 async def sendCreateRoads(net_file):
     net = sumolib.net.readNet(net_file)
     for edge in net.getEdges():
@@ -349,17 +253,17 @@ async def websocket_simulation(websocket: WebSocket):
                 "--proj.utm", "true",
                 "--keep-edges.by-type", sumo_types, # <--- Mantiene solo estos tipos
                 "--remove-edges.isolated", "true"
-        ], check=True)
+            ], check=True)
         else:
             subprocess.run([
                 os.path.join(sumo_home, "bin", "netconvert"),
                 "--osm-files", osm_file,
                 "--output-file", net_file,
-            "--geometry.remove", "true",
-            "--proj.utm", "true",
-            "--keep-edges.by-type", sumo_types, # <--- Mantiene solo estos tipos
-            "--remove-edges.isolated", "true"
-        ], check=True)
+                "--geometry.remove", "true",
+                "--proj.utm", "true",
+                "--keep-edges.by-type", sumo_types, # <--- Mantiene solo estos tipos
+                "--remove-edges.isolated", "true"
+            ], check=True)
 
 
 
@@ -382,9 +286,8 @@ async def websocket_simulation(websocket: WebSocket):
                 "-n", net_file,
                 "-r", route_file,
                 "-e", str(duration_sec),  # Simular 3600 segundos de tráfico
-            "--period", str(period), # Aparece un coche cada 0.5 segundos
-            "--fringe-factor", "1000"
-            
+                "--period", str(period), # Aparece un coche cada 0.5 segundos
+                "--fringe-factor", "10000"
             ], check=True)  
         else:
             procesoTraffic=subprocess.run([
@@ -392,10 +295,10 @@ async def websocket_simulation(websocket: WebSocket):
                 "-n", net_file,
                 "-r", route_file,
                 "-e", str(duration_sec),  # Simular 3600 segundos de tráfico
-            "--period", str(period), # Aparece un coche cada 0.5 segundos
-            "--fringe-factor", "1000"
-            
+                "--period", str(period), # Aparece un coche cada 0.5 segundos
+                "--fringe-factor", "10000"
             ], check=True)  
+
         print("Tráfico generado correctamente para "+ str(num_vehicles) + " vehiculos") 
         await websocket.send_json({"mensaje":"Tráfico generado correctamente para "+ str(num_vehicles) + " vehiculos"})
 
@@ -453,11 +356,38 @@ async def websocket_simulation(websocket: WebSocket):
             
             # Ejecutar la simulación paso a paso
             step = 0
-            max_steps = duration_sec  # Duracion de la simulación
-            
-            while step < max_steps and traci.simulation.getMinExpectedNumber() > 0:
+            await websocket.send_json({"simulationState":"1"})
+            while step < duration_sec and traci.simulation.getMinExpectedNumber() > 0:
                 try:
                     traci.simulationStep()  # Avanzar un paso
+
+                    try:
+                        # Intentamos leer un mensaje sin bloquear la simulación (timeout corto)
+                        data = await asyncio.wait_for(websocket.receive_json(), timeout=0.01)
+                        
+                        if data.get("action") == "close_edge":
+                            edge_id = data.get("edge_id")
+                            await websocket.send_json({"calle_cerrada":"Cerrando calle " + edge_id})
+                            try:
+                                # Lógica de cierre en SUMO
+                                traci.edge.setAllowed(edge_id, [])  # Prohibir paso
+                                traci.edge.setEffort(edge_id, 999999) # Avisar al GPS
+                            
+                            
+                                # Forzar rerouting a los coches que ya están en el mapa
+                                for veh_id in traci.vehicle.getIDList():
+                                    traci.vehicle.rerouteTraveltime(veh_id)
+                                await websocket.send_json({"calle_cerrada":"Calle cerrada correctamente " + edge_id})
+                            except Exception as e:
+                                await websocket.send_json({"calle_cerrada":"Error al cerrar la calle " + edge_id + " " + str(e)})
+                            
+                            # Confirmar al frontal
+                            await websocket.send_json({"type": "status", "msg": f"Calle {edge_id} cerrada"})
+                    
+                    except asyncio.TimeoutError:
+                        # No hay mensajes nuevos, seguimos la simulación
+                        pass
+
                     
                     vehicles_at_step = []
                     for veh in traci.vehicle.getIDList():
@@ -481,8 +411,10 @@ async def websocket_simulation(websocket: WebSocket):
                             "time": step
                         }
 
-                        await websocket.send_json(vehiculo)
+                        await websocket.send_json({"vehiculo":vehiculo})
                     
+                    await asyncio.sleep(0.1)
+
                     step += 1
                 except traci.TraCIException as e:
                     if "has no valid route" in str(e):
@@ -494,6 +426,7 @@ async def websocket_simulation(websocket: WebSocket):
             traci.close()
             print("Simulación finalizada correctamente")
             await websocket.send_json({"mensaje":"Simulación finalizada correctamente"})
+            await websocket.send_json({"simulationState":"0"})
             await websocket.close()
             
                 
@@ -505,18 +438,6 @@ async def websocket_simulation(websocket: WebSocket):
             if traci.isLoaded():
                 traci.close()
             raise HTTPException(status_code=500, detail=str(e))
-
-# WEBSOCKET PARA PROBAR LA CONEXIÓN
-@app.websocket("/ws/test")
-async def test_websocket(websocket: WebSocket):
-    await websocket.accept()
-    mensaje = MensajeSocket(
-        mensaje="Conexión exitosa",
-        color="#00FF00",
-        tipo="success"
-    )
-    await websocket.send_json(mensaje.dict())
-    await websocket.close()
 
 # WEBSOCKET PARA OBTENER LAS CARRETERAS
 @app.websocket("/ws/getRoads")
@@ -561,7 +482,8 @@ async def get_roads_websocket(websocket: WebSocket):
                 "--geometry.remove", "true",
                 "--proj.utm", "true",
                 "--keep-edges.by-type", sumo_types, # <--- Mantiene solo estos tipos
-                "--remove-edges.isolated", "true"
+                "--remove-edges.isolated", "true",
+                "--output.street-names", "true"
             ], check=True)  
             print("Red de SUMO generada correctamente")
             # 3. EN LUGAR DE LLAMAR A net2geojson.py, USAMOS NUESTRA FUNCIÓN
@@ -581,7 +503,8 @@ async def get_roads_websocket(websocket: WebSocket):
                     "nombre": edge.getName() or "Calle sin nombre",
                     "tipo": edge.getType(),
                     "velocidad_max": edge.getSpeed() * 3.6, # Convertir m/s a km/h
-                    "carriles": edge.getLaneNumber()
+                    "carriles": edge.getLaneNumber(),
+                    "prohibida": False
                 }
 
                 feature = {
